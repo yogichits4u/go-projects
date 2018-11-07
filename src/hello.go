5c2df4eaa64ec7c5f307a396fbf3c9e6808ee258
yogichits4u <yogesh.chithore@gmail.com>
2018-11-06 21:15:57 -0600

    time

--- src/hello.go
+++ src/hello.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("Hello", 1)
+	time.Sleep(time.Second)
+	fmt.Println("Test")
 }
