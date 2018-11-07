8b6a4ab1f89ffd9fd775a3d68624f9bf76c03687
yogichits4u <yogesh.chithore@gmail.com>
2018-11-06 22:10:14 -0600

    temp

--- src/hello.go
+++ src/hello.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"time"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("Hello", 1)
-	time.Sleep(time.Second)
-	fmt.Println("Test")
+
+	i := 0
+	for i < 2 {
+		fmt.Printf(". ")
+		time.Sleep(time.Second)
+		i++
+	}
+	fmt.Println("\nTest")
+
+	absPath, _ := filepath.Abs("data/sample.dat")
+	dat, err := ioutil.ReadFile(absPath)
+	//dat, err := ioutil.ReadFile("/home/yogeshc/go/src/github.com/go-projects/data/sample.dat")
+	check(err)
+	fmt.Print(string(dat))
+
+	file, err := os.Open(absPath)
+	check(err)
+	defer file.Close()
+
+	fmt.Println(file.Name())
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		if len(scanner.Text()) > 15 {
+			fmt.Println(scanner.Text())
+		}
+
+	}
 }
