bdc618cbece4e4bda0f77bbc7f550140fdd38e8f
Yogesh Chithore <yogesh.chithore@amdocs.com>
2018-11-08 10:58:27 -0600

    .

--- src/hello.go
+++ src/hello.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"time"
 )
 
 func check(e error) {
@@ -18,17 +17,6 @@ func check(e error) {
 }
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("Hello", 1)
-
-	i := 0
-	for i < 2 {
-		fmt.Printf(". ")
-		time.Sleep(time.Second)
-		i++
-	}
-	fmt.Println("\nTest")
-
 	absPath, _ := filepath.Abs("data/sample.dat")
 	dat, err := ioutil.ReadFile(absPath)
 	//dat, err := ioutil.ReadFile("/home/yogeshc/go/src/github.com/go-projects/data/sample.dat")
